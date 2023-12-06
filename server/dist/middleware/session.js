0990895d2df7af06fafe1f238b114e991328dcf9
jayytruthh <68393753+jayytruthh@users.noreply.github.com>
2023-12-05 23:37:04 -0800

    we fixed it lol

---
+++ server/dist/middleware/session.js
@@ -0,0 +1,23 @@
+import PGSimple from "connect-pg-simple";
+import session from "express-session";
+import { nanoid } from "nanoid";
+import { db } from "../db/index.js";
+const PGSession = PGSimple(session);
+const sessionMiddleware = session({
+    store: new PGSession({ pool: db, createTableIfMissing: true }),
+    secret: process.env.SESSION_SECRET || "degenvsdegen",
+    resave: false,
+    saveUninitialized: false,
+    name: "degen",
+    proxy: true,
+    cookie: {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        secure: true,
+        httpOnly: true,
+        sameSite: "none"
+    },
+    genid: function () {
+        return nanoid(21);
+    }
+});
+export default sessionMiddleware;
--- server/dist/server.js
+++ server/dist/server.js
@@ -1,58 +1,51 @@
 import cors from "cors";
 import "dotenv/config";
-import type { NextFunction, Request, Response } from "express";
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-
 import { INIT_TABLES, db } from "./db/index.js";
 import session from "./middleware/session.js";
 import routes from "./routes/index.js";
 import { init as initSocket } from "./socket/index.js";
-
 const corsConfig = {
-    origin:'https://degen-arcade-client.vercel.app',
+    origin: 'https://degen-arcade-client.vercel.app',
     credentials: true,
 };
-
 const app = express();
 const server = createServer(app);
-
 // database
 await db.connect();
 db.query(INIT_TABLES, (err) => {
     if (err) {
         console.error(err);
-    } else {
+    }
+    else {
         console.log("Tables initialized");
     }
 });
-
 // middleware
 app.use(cors(corsConfig));
 app.use(express.json());
 app.set("trust proxy", 1);
 app.use(session);
 app.use("/v1", routes);
-
 // socket.io
 export const io = new Server(server, { cors: corsConfig, pingInterval: 30000, pingTimeout: 50000 });
 io.use((socket, next) => {
-    session(socket.request as Request, {} as Response, next as NextFunction);
+    session(socket.request, {}, next);
 });
 io.use((socket, next) => {
     const session = socket.request.session;
     if (session && session.user) {
         next();
-    } else {
+    }
+    else {
         console.log("io.use: no session");
         socket.disconnect();
     }
-}); 
+});
 initSocket();
-
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
     console.log(`degen api server listening on :${port}`);
-});  
- 
+});
